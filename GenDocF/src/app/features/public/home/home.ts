2ee1495bbb702213a6333bcf7ea8651514d1e2ea
kouassi04 <kouassisamuelo226@gmail.com>
2026-01-31 03:28:21 +0100

    Amélioration UX/UI Front : Refonte selon retours client

--- GenDocF/src/app/features/public/home/home.ts
+++ GenDocF/src/app/features/public/home/home.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
+
+  categories = [
+    {
+      title: 'Attestations & Déclarations',
+      description: 'Attestation sur l’honneur, hébergement, domicile, etc.',
+      icon: 'description'
+    },
+    {
+      title: 'Résiliations & Contrats',
+      description: 'Assurance, abonnement, logement, services.',
+      icon: 'assignment_return'
+    },
+    {
+      title: 'Réclamations & Litiges',
+      description: 'Courriers de plainte, contestation et mise en demeure.',
+      icon: 'gavel'
+    },
+    {
+      title: 'Travail & Études',
+      description: 'Demande de stage, congé, attestation employeur.',
+      icon: 'school'
+    },
+    {
+      title: 'Vie quotidienne',
+      description: 'Courriers administratifs et démarches personnelles.',
+      icon: 'home'
+    },
+    {
+      title: 'Autres documents',
+      description: 'Modèles personnalisables selon vos besoins.',
+      icon: 'folder'
+    }
+  ];
 
 }
